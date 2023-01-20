db525cf15bbbaa9c7e8a3c21a8e78bf16ec5ee20
maldisco <filipe.sf04@gmail.com>
2023-01-20 09:30:55 -0300

    add read for model mudanca

--- backend/routes/mudancas.js
+++ backend/routes/mudancas.js
@@ -8,26 +8,41 @@ router.post('/mudar/:id/:variacao', (req, res, next) => {
     turmaId: req.params.id,
     data: new Date(),
     variacao: req.params.variacao
-  });
-  mudanca.save().then(newMudanca => {
-    res.status(201).json({
-      message: 'Mudanca registrada!',
-      mudanca: {
-        id: newMudanca._id,
-        turmaId: newMudanca.userId,
-        data: newMudanca.data,
-        variacao: newMudanca.variacao
-      }
-    });
-  }, err => {
-    res.json({
+  })
+  mudanca.save().then(
+    newMudanca => {
+      res.status(201).json({
+        message: 'Mudanca registrada!',
+        mudanca: {
+          id: newMudanca._id,
+          turmaId: newMudanca.userId,
+          data: newMudanca.data,
+          variacao: newMudanca.variacao
+        }
+      })
+    },
+    err => {
+      res.json({
         message: err
-    });
-  });
-});
+      })
+    }
+  )
+})
 
 router.get('/mudancas/:id', (req, res, next) => {
-
+  Mudanca.find({ turmaId: req.params.id }).then(
+    data => {
+      res.status(200).json({
+        message: 'Mudancas retornadas com sucesso',
+        mudancas: data
+      })
+    },
+    err => {
+      res.json({
+        message: err
+      })
+    }
+  )
 })
 
 module.exports = router
